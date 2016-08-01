94fce35cccf18d841c1043eb1152122f8b29554b
Joshua Chen Zhao <u5516240@anu.edu.au>
2016-08-01 23:33:50 +1000

    Lab test 1 practices, Hello world!

---
+++ src/comp1110/labtest1/HelloWorld.java
@@ -0,0 +1,10 @@
+package comp1110.labtest1;
+
+/**
+ * Created by joshuazhao on 1/08/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
