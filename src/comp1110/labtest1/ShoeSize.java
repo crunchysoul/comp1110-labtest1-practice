4cabf90ee89cd6ce8ea8352ab08a6277d934f309
Joshua Chen Zhao <u5516240@anu.edu.au>
2016-08-01 23:56:25 +1000

    Lab test 1 practices, Shoesize

--- src/comp1110/labtest1/ShoeSize.java
+++ src/comp1110/labtest1/ShoeSize.java
@@ -1,7 +1,15 @@
 package comp1110.labtest1;
 
+import java.util.Scanner;
+
 /**
  * Created by joshuazhao on 1/08/2016.
  */
 public class ShoeSize {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println();
+        double a = in.nextDouble();
+        System.out.println(Math.round(a*5.0));
+    }
 }
